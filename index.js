c4e82c854c6ef5e21ef1b3d5939437d80e07093d
Daniel Kapusta <kapusta@gmail.com>
2016-09-29 20:58:50 -0400

    commented out the daemon section since the listener interfaces with an already running gpsd

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 var gpsd = require('node-gpsd');
 
-var daemon = new gpsd.Daemon({
-    program: 'gpsd',
-    device: '/dev/ttyAMA0',
-    port: 2947,
-    pid: '/tmp/gpsd.pid',
-    logger: {
-        info: function() {},
-        warn: console.warn,
-        error: console.error
-    }
-});
-
-
-daemon.start(function(arg) {
-  console.log('Started', arg);
-});
-
+// var daemon = new gpsd.Daemon({
+//     program: 'gpsd',
+//     device: '/dev/ttyAMA0',
+//     port: 2947,
+//     pid: '/tmp/gpsd.pid',
+//     logger: {
+//         info: function() {},
+//         warn: console.warn,
+//         error: console.error
+//     }
+// });
+//
+//
+// daemon.start(function(arg) {
+//   console.log('Started', arg);
+// });
+//
 
 var listener = new gpsd.Listener({
     port: 2947,
@@ -37,4 +37,4 @@ if (listener.isConnected()) {
   console.log('listening!');
 }
 
-listener.watch(options);
+listener.watch();
