4420892447d0e514fb6f5fdb8861af24ddbf9181
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-10-16 21:27:40 -0400

    conforms to xo rules, sends out mqkey if it exists

--- index.js
+++ index.js
@@ -1,60 +1,58 @@
 var argv = require('yargs').argv;
+var Server = require('ws').Server;
 var daemon = require('./lib/daemon.js');
-var server = require('ws').Server
-var wss = new server({ port: (argv.port) ? argv.port : '9000' });
 
-if (argv.mq) {
-  var mqkey = require('./lib/mqkey.js').consumerKey;
-}
-
-const location = {
-  current: {},
+const mqkey = {
+  consumerKey: ''
 };
 
+const location = {
+  current: {}
+};
+
+if (argv.mq) {
+  mqkey.consumerKey = require('./lib/mqkey.js').consumerKey;
+}
+
+var wss = new Server({
+  port: (argv.port) ? argv.port : '9000'
+});
+
 const dmon = daemon.init({
-  'port': (argv.daemonPort) ? argv.daemonPort : 2947
+  port: (argv.daemonPort) ? argv.daemonPort : 2947
 });
 dmon.start();
 const listener = daemon.listen({});
 
-
-wss.on('connection', (socket) => {
-
+// set up server listener
+wss.on('connection', socket => {
   console.log('new websocket connection, (', wss.clients.length, ' total)');
 
   // send out the location data on an interval
-  var intervalId = setInterval(function() {
+  var intervalId = setInterval(function () {
     socket.send(JSON.stringify(location.current));
   }, 2000);
 
-  socket.on('message', (data, flags) => {
+  socket.on('message', data => { // (data, flags)
     var parsedData = JSON.parse(data);
-    //console.log('new message from client', parsedData);
+    // console.log('new message from client', parsedData);
 
-    // if the data contains an action, try to do that action (this needs more thought)
-    // if (parsedData.action) {
-    //   console.log('trying', parsedData.action);
-    //   try {
-    //     daemon[parsedData.action].call(this, dmon);
-    //   }
-    //   catch (e) {
-    //     console.error(e);
-    //   }
-    // }
+    // there's room for more protocols here...
+    if (parsedData.action === 'getConsumerKey' && mqkey.consumerKey) {
+      socket.send(JSON.stringify(mqkey));
+    }
   });
 
   socket.on('close', () => {
     clearInterval(intervalId);
     console.log('websocket connection closed, (', wss.clients.length, ' remain)');
   });
-
 });
 
-
-
+// set up event listeners
 listener.on('connected', () => console.log('listener is conected'));
-listener.on('DEVICE', (data) => console.log('device', data));
-listener.on('TPV', (tpvData) => {
+listener.on('DEVICE', data => console.log('device', data));
+listener.on('TPV', tpvData => {
   location.current = tpvData;
 });
 
