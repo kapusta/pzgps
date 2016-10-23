e8070602622de69e50246d6a01c049e186049169
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-10-22 20:16:57 -0400

    configured to use a Navbutton class

--- reactjs/src/navbar/navbar.jsx
+++ reactjs/src/navbar/navbar.jsx
@@ -2,18 +2,34 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import styles from '../index.scss';
 import navbarStyles from './navbar.scss';
+import Navbutton from '../navbutton/navbutton.jsx';
 
 const buttonStyles = {
   margin: '10px 10px 10px 0px'
 };
 
+const buttons = [{
+  id: 1,
+  text: 'About'
+}, {
+  id: 2,
+  text: 'GPS Data'
+}, {
+  id: 3,
+  text: 'MapQuest'
+}];
+
+
 class Navbar extends React.Component {
   render() {
+    var btns = buttons.map(function(b) {
+      return (
+        <Navbutton key={b.id}>{b.text}</Navbutton>
+      )
+    });
     return (
       <div>
-        <button className="btn btn-primary" style={buttonStyles}>About</button>
-        <button className="btn btn-primary" style={buttonStyles}>GPS Data</button>
-        <button className="btn btn-primary" style={buttonStyles}>MapQuest</button>
+        {btns}
       </div>
     )
   }
