d9f3d4d0a6e498b1ad9a7f01b9711fc16a00e640
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-10-22 20:18:11 -0400

    initial commit of Navbutton class and styles

---
+++ reactjs/src/navbutton/navbutton.jsx
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import styles from '../index.scss';
+import buttonStyles from './navbutton.scss';
+
+let classNames = 'btn btn-primary ' + buttonStyles.button;
+
+class Navbutton extends React.Component {
+
+
+  render() {
+    return (
+      <button className={classNames}>{this.props.children}</button>
+    )
+  }
+}
+
+export default Navbutton;
