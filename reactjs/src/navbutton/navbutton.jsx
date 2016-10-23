d9436a1178288e8420f836741d965ff9bc9dba08
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-10-22 20:42:26 -0400

    adding classNames via template strings

--- reactjs/src/navbutton/navbutton.jsx
+++ reactjs/src/navbutton/navbutton.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 import styles from '../index.scss';
 import buttonStyles from './navbutton.scss';
 
-let classNames = 'btn btn-primary ' + buttonStyles.button;
+//let classNames = 'btn btn-primary ' + buttonStyles.button;
 
 class Navbutton extends React.Component {
 
 
   render() {
     return (
-      <button className={classNames}>{this.props.children}</button>
+      //<button className={classNames}>{this.props.children}</button>
+      <button className={`btn btn-primary ${buttonStyles.button}`}>{this.props.children}</button>
     )
   }
 }
