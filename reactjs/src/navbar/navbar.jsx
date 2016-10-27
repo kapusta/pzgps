e8b60850626737a137bc2501324b174151292a69
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-10-27 18:18:45 -0400

    removed method that lives elsewhere now, change click handler ref

--- reactjs/src/navbar/navbar.jsx
+++ reactjs/src/navbar/navbar.jsx
@@ -1,5 +1,6 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import { render } from 'react-dom';
 import styles from '../index.scss';
 import navbarStyles from './navbar.scss';
 import Navbutton from '../navbutton/navbutton.jsx';
@@ -9,30 +10,20 @@ const buttonStyles = {
 };
 
 class Navbar extends React.Component {
-
   constructor(props) {
     super(props);
-    this.state = {
-      componentName: 'About'
-    };
   }
-
-  handleClick = n => {
-    this.setState({
-      componentName: n
-    });
-  }
-
   render() {
     return (
-      <div className={navbarStyles.navbar}>
-        <Navbutton handleClick={this.handleClick} section="About">About</Navbutton>
-        <Navbutton handleClick={this.handleClick} section="GpsData">GPS Data</Navbutton>
-        <Navbutton handleClick={this.handleClick} section="MapQuest">MapQuest</Navbutton>
+      <div>
+        <div className={navbarStyles.navbar}>
+          <Navbutton handleClick={this.props.handleClick} section="About">About</Navbutton>
+          <Navbutton handleClick={this.props.handleClick} section="GpsData">GPS Data</Navbutton>
+          <Navbutton handleClick={this.props.handleClick} section="MapQuest">MapQuest</Navbutton>
+        </div>
       </div>
     )
   }
-
 }
 
 export default Navbar;
