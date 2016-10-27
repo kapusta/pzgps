7515fc3dd667b74e7d5838e36a74f67e245788db
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-10-27 18:17:28 -0400

    constructor, moved state of content changes here

--- reactjs/src/app/app.jsx
+++ reactjs/src/app/app.jsx
@@ -1,12 +1,28 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import Navbar from '../navbar/navbar.jsx';
+import ContentBox from '../ContentBox/ContentBox.jsx';
+
 // maybe set up the WebSocket here?
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      componentName: 'About'
+    };
+  }
+  // method is passed down to app -> navbar -> navbutton
+  handleClick = n => {
+    this.setState({
+      componentName: n
+    });
+  }
   render() {
     return (
       <div>
-        {this.props.children}
+        <Navbar handleClick={this.handleClick}/>
+        <ContentBox content={this.state.componentName} />
       </div>
     )
   }
