bfe18fc83d59ad8f16cceab51fc52dff2c6ab6cc
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-10-30 20:18:48 -0400

    moved deps order

--- reactjs/src/MapQuest/MapQuest.jsx
+++ reactjs/src/MapQuest/MapQuest.jsx
@@ -1,5 +1,5 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from './mapquest.scss';
 
 class MapQuest extends React.Component {
