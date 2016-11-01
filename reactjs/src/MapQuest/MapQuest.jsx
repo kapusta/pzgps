79960a24c5294e946b808229946ceff6226b6622
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-10-31 21:25:23 -0400

    setState when the component mounts to formulate the staticmap url, for #2

--- reactjs/src/MapQuest/MapQuest.jsx
+++ reactjs/src/MapQuest/MapQuest.jsx
@@ -5,15 +5,25 @@ import styles from './mapquest.scss';
 class MapQuest extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      mapUrl: ''
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      mapUrl: 'https://www.mapquestapi.com/staticmap/v4/getmap?key='
+        + this.props.consumerKey
+        + '&center=' + this.props.gpsData.lat + ',' + this.props.gpsData.lon
+        + '&zoom=10&size=640,480&type=map&imagetype=png&pois=blue,'
+        + this.props.gpsData.lat + ',' + this.props.gpsData.lon
+    });
   }
   render() {
     return (
       <div>
         <div className="card card-block">
           <h3 className="card-title">MapQuest</h3>
-          <p>
-            Eventually a MapQuest staticmap will go here.
-          </p>
+          <img src={this.state.mapUrl} />
         </div>
       </div>
     )
