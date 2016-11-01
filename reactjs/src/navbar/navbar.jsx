2afd34a8ccafe3eb8078d4381420b3114c31a344
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-11-01 19:07:20 -0400

    remove unused buttonStyles, some spacing for clarity

--- reactjs/src/navbar/navbar.jsx
+++ reactjs/src/navbar/navbar.jsx
@@ -5,10 +5,6 @@ import styles from '../index.scss';
 import navbarStyles from './navbar.scss';
 import Navbutton from '../navbutton/navbutton.jsx';
 
-const buttonStyles = {
-  margin: '10px 10px 10px 0px'
-};
-
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
@@ -17,9 +13,13 @@ class Navbar extends React.Component {
       return (
         <div>
           <div className={navbarStyles.navbar}>
+
             <Navbutton handleClick={this.props.handleClick} section="About">About</Navbutton>
-            {this.props.gpsData ? <Navbutton handleClick={this.props.handleClick} section="GpsData">GPS Data</Navbutton> : ''}
+
+            {(this.props.gpsData) ? <Navbutton handleClick={this.props.handleClick} section="GpsData">GPS Data</Navbutton> : ''}
+
             {(this.props.gpsData && this.props.consumerKey) ? <Navbutton handleClick={this.props.handleClick} section="MapQuest">MapQuest</Navbutton> : ''}
+
           </div>
         </div>
       )
