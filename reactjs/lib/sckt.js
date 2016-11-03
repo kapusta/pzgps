e58b69637193708d9188f13b2189e61f3a1023cb
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-11-03 19:39:03 -0400

    return the rejection onerror

--- reactjs/lib/sckt.js
+++ reactjs/lib/sckt.js
@@ -2,16 +2,15 @@ const connect = serverUrl => {
   return new Promise((resolve, reject) => {
     var socket = new WebSocket(serverUrl);
     socket.onopen = e => {
+      console.log('Connected.');
       return resolve(socket);
     };
-  });
-};
-
-const disconnect = socket => {
-  socket.close();
+    socket.onerror = e => {
+      return reject(e);
+    }
+  }); // don't need a catch clause here because of the onerror above
 };
 
 module.exports = {
-  connect: connect,
-  disconnect: disconnect
+  connect: connect
 }
