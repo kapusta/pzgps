bb5c4d43bd733a5c01079f9d449202de7a887cee
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-11-09 22:37:46 -0500

    prebinding of method to the proper context for #10

--- front-ends/reactjs/src/app/app.jsx
+++ front-ends/reactjs/src/app/app.jsx
@@ -54,6 +54,7 @@ const setUpSocket = Component => {
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       componentName: 'About',
       gpsData: {},
