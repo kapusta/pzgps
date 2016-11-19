dfd3bd83e3a7decdcc066319a6914ba9c0d0ca1f
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-11-18 20:45:35 -0500

    experimenting with porting to preact for #11

---
+++ front-ends/preact/src/components/About/About.jsx
@@ -0,0 +1,22 @@
+//import styles from '../index.scss';
+import { h, Component } from 'preact';
+
+class About extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <div className="card card-block">
+          <h3 className="card-title">About</h3>
+          <p>
+            This is a ReactJS implementation of the GPS front end. It's based on the <a href="https://github.com/alicoding/react-webpack-babel">react-webpack-babel</a> project and uses <a href="https://www.npmjs.com/package/react-websocket">react-websocket</a> for the connection to the server.
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default About;
--- front-ends/preact/src/components/app.js
+++ front-ends/preact/src/components/app.js
@@ -4,7 +4,7 @@ import { Router } from 'preact-router';
 import Header from './header';
 import Home from './home';
 import Profile from './profile';
-
+import About from './About/About.jsx';
 
 export default class App extends Component {
 	/** Gets fired when the route changes.
@@ -23,6 +23,7 @@ export default class App extends Component {
 					<Home path="/" />
 					<Profile path="/profile/" user="me" />
 					<Profile path="/profile/:user" />
+          <About path="/about" />
 				</Router>
 			</div>
 		);
