e7f9da9c5ca053000a4fe1449b4167bf0e81ec8f
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-11-19 23:45:30 -0500

    adding Navbutton, need to clean up and make sense for preact vs react for #11

---
+++ front-ends/preact/src/components/NavButton/NavButton.jsx
@@ -0,0 +1,30 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import buttonStyles from './navbutton.scss';
+
+class Navbutton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick = e => {
+    console.log('button click event', e);
+    this.props.handleClick(this.props.section);
+  }
+  render() {
+    return (
+      <button
+        className={`btn ${buttonStyles.button}` + ((this.props.isActive) ? ' btn-primary' : '')}
+        onClick={this.handleClick}
+      >{this.props.children}</button>
+    )
+  }
+}
+
+Navbutton.propTypes = {
+  isActive: React.PropTypes.bool,
+  handleClick: React.PropTypes.func,
+  section: React.PropTypes.string
+};
+
+export default Navbutton;
