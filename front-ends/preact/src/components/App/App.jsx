d88a210f3bb3318c9f69558be5fdb5f846f79492
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-11-25 22:51:49 -0500

    move setUpSocket method into sckt.js

--- front-ends/preact/src/components/App/App.jsx
+++ front-ends/preact/src/components/App/App.jsx
@@ -5,52 +5,6 @@ import ContentBox from '../ContentBox/ContentBox.jsx';
 
 const serverUrl = 'ws://circ.local:9000'; // you'll want to change this
 
-// maybe move this into sckt.js
-const setUpSocket = Component => {
-
-  sckt.connect(serverUrl).then(socket => {
-
-    Component.setState({
-      socket
-    });
-
-    socket.send(JSON.stringify({
-      'action': 'getConsumerKey'
-    }));
-
-    socket.onclose = e => {
-      // https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
-      console.error('The WebSocket connection closed', e);
-      console.log('Trying to reconnect...');
-      window.setTimeout(() => {
-        setUpSocket(Component);
-      }, 5000);
-    };
-
-    socket.onmessage = e => {
-      let data = JSON.parse(e.data);
-      if (data.consumerKey) {
-        Component.setState({
-          consumerKey: data.consumerKey
-        });
-      } else {
-        Component.setState({
-          gpsData: data
-        });
-      }
-    };
-
-  }).catch((err) => {
-    console.error('could not connect to WebSocket server', err);
-    console.log('Trying to reconnect...');
-    setTimeout(() => {
-      setUpSocket(Component);
-    }, 5000);
-  });
-
-};
-
-
 class App extends Component {
   constructor(props) {
     super(props);
@@ -63,7 +17,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setUpSocket(this);
+    sckt.setUpSocket(serverUrl, this);
   }
 
   componentWillUnmount() {
--- front-ends/preact/src/lib/sckt.js
+++ front-ends/preact/src/lib/sckt.js
@@ -11,6 +11,52 @@ const connect = serverUrl => {
   }); // don't need a catch clause here because of the onerror above
 };
 
+// TODO: not purely functional
+const setUpSocket = (serverUrl, Component) => {
+
+  connect(serverUrl).then(socket => {
+
+    Component.setState({
+      socket
+    });
+
+    socket.send(JSON.stringify({
+      'action': 'getConsumerKey'
+    }));
+
+    socket.onclose = e => {
+      // https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent
+      console.error('The WebSocket connection closed', e);
+      console.log('Trying to reconnect...');
+      window.setTimeout(() => {
+        setUpSocket(Component);
+      }, 5000);
+    };
+
+    socket.onmessage = e => {
+      let data = JSON.parse(e.data);
+      if (data.consumerKey) {
+        Component.setState({
+          consumerKey: data.consumerKey
+        });
+      } else {
+        Component.setState({
+          gpsData: data
+        });
+      }
+    };
+
+  }).catch((err) => {
+    console.error('could not connect to WebSocket server', err);
+    console.log('Trying to reconnect...');
+    setTimeout(() => {
+      setUpSocket(Component);
+    }, 5000);
+  });
+
+};
+
 module.exports = {
-  connect
+  connect,
+  setUpSocket
 };
