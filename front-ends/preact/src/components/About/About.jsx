1dab04857d664f6d46ff2477dac18fd2d7428342
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-11-26 22:40:09 -0500

    fixed info in the About component

--- front-ends/preact/src/components/About/About.jsx
+++ front-ends/preact/src/components/About/About.jsx
@@ -12,7 +12,7 @@ class About extends Component {
         <div className="card card-block">
           <h3 className="card-title">About</h3>
           <p>
-            This is a ReactJS implementation of the GPS front end. It's based on the <a href="https://github.com/alicoding/react-webpack-babel">react-webpack-babel</a> project and uses <a href="https://www.npmjs.com/package/react-websocket">react-websocket</a> for the connection to the server.
+            This is a <a href="https://preactjs.com/">Preact</a> implementation of the GPS front end based on the <a href="https://github.com/developit/preact-boilerplate">preact-boilerplate</a> project. If the server on the pizero is set up correctly, a GPS Data and MapQuest button will appear above.
           </p>
         </div>
       </div>
