28fb90a29e18a50417427f66b99aba158252f490
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-12-06 23:18:21 -0500

    some markup for more content space

--- front-ends/preact/src/components/About/About.jsx
+++ front-ends/preact/src/components/About/About.jsx
@@ -9,11 +9,23 @@ class About extends Component {
   render() {
     return (
       <div>
-        <div className="card card-block">
-          <h3 className="card-title">About</h3>
-          <p>
-            This is a <a href="https://preactjs.com/">Preact</a> implementation of the GPS front end based on the <a href="https://github.com/developit/preact-boilerplate">preact-boilerplate</a> project. If the server on the pizero is set up correctly, a GPS Data and MapQuest button will appear above.
-          </p>
+        <div className="col-lg-6">
+          <h4 className="card-title">About</h4>
+          <div className="card card-block">
+            <p>
+              This is a <a href="https://preactjs.com/">Preact</a> implementation of the GPS front end based on the <a href="https://github.com/developit/preact-boilerplate">preact-boilerplate</a> project.
+            </p>
+            <p>
+              If the server on the pizero is set up correctly, more buttons will appear above after establishing the WebSocket connection.
+            </p>
+          </div>
+        </div>
+
+        <div className="col-lg-6">
+          <h4 className="card-title">Title</h4>
+          <div className="card card-block">
+            More content here.
+          </div>
         </div>
       </div>
     );
