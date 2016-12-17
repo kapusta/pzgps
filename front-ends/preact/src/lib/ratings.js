ad2bdb8353036aa9f5bb21c78c650863afa0b70e
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-12-16 19:53:12 -0500

    syntax

--- front-ends/preact/src/lib/ratings.js
+++ front-ends/preact/src/lib/ratings.js
@@ -1,19 +1,21 @@
 /**
   @see https://en.wikipedia.org/wiki/Grade_(climbing)
 */
-const ratings = {
-  'yds': new Array(16).fill(0).map((val, idx) => {
-    return {
-      'value': '5.' + idx,
-      'label': '5.' + idx
-    };
-  }),
-  'hueco': new Array(18).fill(0).map((val, idx) => {
-    return {
-      'value': 'V' + idx,
-      'label': 'V' + idx
-    };
-  })
-};
+const yds = new Array(16).fill(0).map((val, idx) => {
+  return {
+    'value': '5.' + idx,
+    'label': '5.' + idx
+  };
+});
 
-export default ratings;
+const hueco = new Array(18).fill(0).map((val, idx) => {
+  return {
+    'value': 'V' + idx,
+    'label': 'V' + idx
+  };
+});
+
+export {
+  yds,
+  hueco
+};
