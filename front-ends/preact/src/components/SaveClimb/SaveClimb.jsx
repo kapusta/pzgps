7df9960deeab89fa34a65ed5fee75a064940dab1
Daniel Kapusta <daniel.kapusta@teamaol.com>
2016-12-21 22:53:11 -0500

    naive support for adding and getting database records via pouchdb, #14

--- front-ends/preact/src/components/SaveClimb/SaveClimb.jsx
+++ front-ends/preact/src/components/SaveClimb/SaveClimb.jsx
@@ -2,15 +2,21 @@ import { h, Component } from 'preact';
 import shortid from 'shortid';
 import Select from 'react-select';
 import classNames from 'classnames/bind';
+import PouchDB from 'pouchdb';
 import styles from './saveclimb.css';
+import conf from '../../lib/conf.js';
 import { yds } from '../../lib/ratings.js';
 import merge from 'lodash/merge';
+
+let dbname = 'routes'
+let db = PouchDB(conf.couchdb + '/' + dbname);
 
 class SaveClimb extends Component {
   constructor(props) {
     super(props);
     this.state = {
       saving: false, // is the page saving data
+      searching: false, // is the page saving data
       route: {
         name: '',
         pitches: 3,
@@ -48,16 +54,49 @@ class SaveClimb extends Component {
   handleChange = evt => {
     let route = merge({}, this.state.route, {[evt.target.id]: evt.target.value});
     this.setState({
+      _rev: null, // kill the db doc _rev value if/when the user changes the name
       route
     });
   }
   logState = () => {
     console.log(this.state);
   }
+  search = () => {
+    let that = this; // gross, need it for the promise closure
+    this.setState({
+      searching: true
+    });
+    // get data based on the name, if found, use the data to
+    // prepopulate the rest of the form
+    db.get(this.state.route.name)
+    .then(function(doc){
+      let route = merge({}, {
+        name: doc.name,
+        pitches: doc.pitches,
+        rating: doc.rating
+      });
+      that.setState({
+        _rev: doc._rev,
+        searching: false,
+        route
+      });
+    })
+    .catch(function(err) {
+      console.error(err);
+      that.setState({
+        searching: false
+      });
+
+    });
+  }
   save = evt => {
     this.setState({
       saving: true
     });
+
+    db.put(merge({
+      _id: (this.state._rev) ? this.state._rev : this.state.route.name
+    }, this.state.route, this.props.gpsData));
 
     this.props.socket.send(JSON.stringify({
       action: 'newRoute',
@@ -69,12 +108,18 @@ class SaveClimb extends Component {
     let labelStyles = cn('col-lg-2 col-form-label', {
       'label': true
     });
-    let loggerStyles = cn('btn btn-sm', {
-      'btn-logger': true
+    let loggerStyles = cn('btn btn-sm mybuttons', {
+      mybuttons: true // add css module styles
     });
-    let saveButtonStyles = cn('btn btn-sm', {
-      'btn-primary': true
+    let searchButtonStyles = cn('btn btn-sm btn-primary', {
+      mybuttons: true // add css module styles
     });
+    let searchButtonIcon = cn('fa', {
+      'fa-search': !this.state.searching,
+      'fa-spinner': this.state.searching,
+      'fa-spin': this.state.searching
+    });
+    let saveButtonStyles = cn('btn btn-sm btn-primary');
     let saveButtonIcon = cn('fa', {
       'fa-floppy-o': !this.state.saving,
       'fa-spinner': this.state.saving,
@@ -126,6 +171,15 @@ class SaveClimb extends Component {
           disabled={this.state.saving}
         >
           <i className={saveButtonIcon} aria-hidden="true"></i> Save
+        </button>
+
+        <button
+          bs-button
+          className={searchButtonStyles}
+          onClick={this.search}
+          disabled={this.state.searching}
+        >
+          <i className={searchButtonIcon} aria-hidden="true"></i> Look Up By Route Name
         </button>
 
         <button
