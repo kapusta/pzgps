bed4bb5fc99b88d5f31958c883690e8bad5c90d3
Daniel Kapusta <daniel.kapusta@teamaol.com>
2017-01-04 21:40:53 -0500

    initial commit, probably needs to be moved, #14

---
+++ front-ends/preact/src/components/ListRoutes/DeleteButton.jsx
@@ -0,0 +1,40 @@
+import { h, Component } from 'preact';
+import proptypes from 'proptypes';
+import classNames from 'classnames/bind';
+import styles from './deletebutton.css';
+
+class DeleteButton extends Component {
+  constructor(props) {
+    super(props);
+  }
+  deleteRoute = () => {
+    this.setState({
+      deleting: true
+    });
+    this.props.removeRoute(this.props.route);
+    // after the delete happens, the parent component will refetch the route list
+    // and deliver that to this component forcing a re-render which removes the
+    // route 'doc' from the view, so no need to reset `deleting` to false
+  }
+  render() {
+    let cn = classNames.bind(styles);
+    let deleteButton = cn('btn btn-sm btn-primary');
+    let deleteButtonIcon = cn('fa', {
+      'fa-trash': !this.props.deleting,
+      'fa-spinner': this.props.deleting,
+      'fa-spin': this.props.deleting
+    });
+    return (
+      <button
+        bs-button
+        className={deleteButton}
+        onClick={this.deleteRoute}
+        disabled={this.state.deleting}
+      >
+        <i className={deleteButtonIcon} aria-hidden="true"></i>
+      </button>
+    );
+  }
+}
+
+export default DeleteButton;
