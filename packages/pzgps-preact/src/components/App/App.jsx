b23d8444f4db22a293a5a05ee36e695dc663bae2
Daniel Kapusta <daniel.kapusta@teamaol.com>
2017-02-27 21:17:13 -0500

    send props into AppFooter for #21

--- packages/pzgps-preact/src/components/App/App.jsx
+++ packages/pzgps-preact/src/components/App/App.jsx
@@ -48,7 +48,12 @@ class App extends Component {
           databases={db.databases}
         />
 
-        <AppFooter />
+        <AppFooter
+          handleClick={this.handleClick}
+          consumerKey={this.state.consumerKey ? true : false}
+          gpsData={Object.keys(this.state.gpsData).length ? true : false}
+          componentName={this.state.componentName}
+        />
       </div>
     );
   }
